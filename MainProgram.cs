aeb257b356d6da97651af947cec08a58cfd0e059
NijikawaSatoki <nijikawasatoki00@outlook.com>
2024-11-18 03:18:33 -0500

    Current code for now :)

--- MainProgram.cs
+++ MainProgram.cs
@@ -5,3 +5,126 @@
  * Niji System - Wamo / ワーモ
  * 
  */
+
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace DiceRoller {
+    class MainProgram {
+        static void Main() {
+            // You'll see in a moment
+            bool developerMode = false;
+            // Ask the user for their name
+            string user = "\u3000";
+            string[,] userNiji = {
+                // English
+                { "Satoki", "Satoki Nijikawa", "Wamo", "Kiyoi", "Niji", "Niji System" },
+                // Japanese
+                { "郷鬼", "虹川郷鬼", "ワーモ", "清", "虹", "虹システム" },
+                // Finnish
+                { "Satoki", "Satoki Nizikava", "Vaamo", "Kijoji", "Nizi", "Nizisysteemi" },
+            };
+            Console.Write("What is the name you go by? (Preferred name, no deadnames please) ");
+            user = Console.ReadLine();
+            if (user == userNiji[0, 0] || user == userNiji[0, 1] || user == userNiji[0, 2] || user == userNiji[0, 3] || user == userNiji[0, 4] || user == userNiji[0, 5]) {
+                developerMode = true;
+            } else if (user == userNiji[1, 0] || user == userNiji[1, 1] || user == userNiji[1, 2] || user == userNiji[1, 3] || user == userNiji[1, 4] || user == userNiji[1, 5]) {
+                developerMode = true;
+            } else if (user == userNiji[2, 0] || user == userNiji[2, 1] || user == userNiji[2, 2] || user == userNiji[2, 3] || user == userNiji[2, 4] || user == userNiji[2, 5]) {
+                developerMode = true;
+            } else {
+                developerMode = false;
+            }
+            // Retrieve date and time
+            DateTime time = DateTime.Now;
+            string dateTimeFileFormat = "yyyyMMdd-HHmmss";
+            string dateTimeGreetFormat = "HH:mm:ss yyyy/MM/dd";
+            string fileTime = time.ToString(dateTimeFileFormat);
+            string timeGreeting = time.ToString(dateTimeGreetFormat);
+            // Create output file
+            char dirSep = '\u3000';
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+                dirSep = '\\';
+            } else {
+                dirSep = '/';
+            }
+            string appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string devNijisProjectDataDir = "\u3000";
+            if (developerMode == true) {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+                    Directory.CreateDirectory($@"{appData}{dirSep}NijiProjectData");
+                    devNijisProjectDataDir = $@"NijiProjectData";
+                } else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
+                    Directory.CreateDirectory($@"{appData}{dirSep}.nijidata");
+                    devNijisProjectDataDir = $@".nijidata";
+                }
+            }
+            string dataDir = @"GenerateRandomNumber";
+            string outFile = $@"YourNumbers-{fileTime}.txt";
+            if (developerMode == true) {
+                dataDir = $@"{devNijisProjectDataDir}{dirSep}GenerateRandomNumber";
+            }
+            string outPath = $@"{appData}{dirSep}{dataDir}{dirSep}{outFile}";
+            string outHead = $"This file was created {timeGreeting}.\n\n\n";
+            Directory.CreateDirectory($@"{appData}{dirSep}{dataDir}");
+            File.AppendAllText(outPath, outHead);
+            // RNG initialisation
+            var rng = new Random();
+            int d6 = rng.Next(1, 7); // genNum == 1 <= x < 7
+            int d20 = rng.Next(1, 21); // genNum == 1 <= x < 21
+            // START!
+            Console.WriteLine($"Welcome! The current date and time is {timeGreeting}!");
+            // Open the file stream
+            TextWriter rngOutput = new StreamWriter(outPath, true);
+            // Ask which dice to roll
+            int rollCount;
+            int dice;
+            Console.WriteLine("Which dice do you want to roll?");
+            Console.WriteLine("1. d6" + "\t" + "2. d20");
+            dice = Convert.ToInt32(Console.ReadLine());
+            switch (dice) {
+                case 1:
+                    rngOutput.WriteLine($"{user} wants to roll a d6.");
+                    // Ask how many times to roll the die
+                    Console.WriteLine("How many times do you want to roll?");
+                    rollCount = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine($"{user} rolls {rollCount}d6!");
+                    rngOutput.WriteLine($"{user} rolls {rollCount}d6!");
+                    // Rolls the die
+                    for (int i = 1; i <= rollCount; i++) {
+                        Console.Write($"{d6}, ");
+                        rngOutput.WriteLine($"Your number is {d6}!");
+                        d6 = rng.Next(1, 7);
+                    }
+                    Console.Write("\n");
+                    break;
+                case 2:
+                    rngOutput.WriteLine($"{user} wants to roll a d20.");
+                    // Ask how many times to roll the die
+                    Console.WriteLine("How many times do you want to roll?");
+                    rollCount = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine($"{user} rolls {rollCount}d20!");
+                    rngOutput.WriteLine($"{user} rolls {rollCount}d20!");
+                    // Rolls the die
+                    for (int i = 1; i <= rollCount; i++) {
+                        Console.Write($"{d20}, ");
+                        rngOutput.WriteLine($"Your number is {d20}!");
+                        d20 = rng.Next(1, 21);
+                    }
+                    Console.Write("\n");
+                    break;
+                // End of switch block
+            }
+            /* The exit routine */
+            // Close the file stream
+            rngOutput.WriteLine($"Hope to see you again soon, {user}!");
+            rngOutput.Close();
+            // Send the user on their way
+            Console.WriteLine($@"Your generated numbers can be found in {outPath}");
+            Console.WriteLine($"Have a nice day, {user}!");
+            Console.WriteLine("Please press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
